f8d4a545e80ba966a54c0135af68805625de1eb7
Akinniyi Ezekiel Wonderful <96802687+Wondersmasher@users.noreply.github.com>
2023-08-21 09:24:08 +0100

    Added mongoose package and connected it to atlas database with mongodbcompass

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 const port = process.env.PORT || 8000;
 
+const DB = process.env.DATABASE_URL;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('connected');
+  });
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
 });
