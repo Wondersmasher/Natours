fa2c312ea896099147d5d9c97e249b4aeff45262
Akinniyi Ezekiel Wonderful <96802687+Wondersmasher@users.noreply.github.com>
2023-08-23 13:05:41 +0100

    removed unused middlewares

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,13 +5,11 @@ const {
   getSingleTour,
   updateSingleTour,
   deleteTour,
-  checkID,
-  checkResponseBody,
 } = require('./../controllers/tourControllers');
 const router = express.Router();
 
-router.param('id', checkID);
-router.route('/').get(getAllTours).post(checkResponseBody, createTour);
+// router.param('id', checkID);
+router.route('/').get(getAllTours).post(createTour);
 router
   .route('/:id')
   .get(getSingleTour)
