ff06b5d75e539b3c652d31b4cf963f766cff03f4
Akinniyi Ezekiel Wonderful <96802687+Wondersmasher@users.noreply.github.com>
2023-08-23 13:03:36 +0100

    removed checkId and CheckResponseBody middlewares

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -1,75 +1,113 @@
-const fs = require('fs');
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-// MiddleWares
-exports.checkID = (req, res, next, val) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Could not find the ID',
-    });
-  }
-  next();
-};
-
-exports.checkResponseBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
-  }
-  next();
-};
+const Tour = require('../model/tourModel');
 
 // Get all the tours
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: { tours },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      data: { tours },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'failed',
+      message: `Error :${error}`,
+    });
+  }
 };
 
 // Create a new Tour
-exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: { tour: newTour },
-      });
-    }
-  );
+exports.createTour = async (req, res) => {
+  try {
+    const tour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: { tour },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'failed',
+      message: `Error :${error}`,
+    });
+  }
 };
-exports.getSingleTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((item) => item.id === id);
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
+exports.getSingleTour = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const tour = await Tour.findById(id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'failed',
+      message: `Error: ${error}`,
+    });
+  }
 };
 
-exports.updateSingleTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    message: 'Updated',
-  });
+exports.updateSingleTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json({
+      status: 'success',
+      message: 'Updated',
+      data: { tour },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'failed',
+      message: `Error: ${error}`,
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'failed',
+      message: `Error: ${error}`,
+    });
+  }
+  // try {
+
+  // } catch (error) {
+  //   res.status(400).json({
+  //     status:'failed'
+  //   })
+  // }
+  // res.status(204).json({
+  //   status: 'success',
+  //   data: null,
+  // });
 };
+
+// MiddleWares
+// exports.checkID = (req, res, next, val) => {
+//   if (req.params.id * 1 > tours.length) {
+//     return res.status(404).json({
+//       status: 'fail',
+//       message: 'Could not find the ID',
+//     });
+//   }
+//   next();
+// };
+
+// exports.checkResponseBody = (req, res, next) => {
+//   if (!req.body.name || !req.body.price) {
+//     return res.status(400).json({
+//       status: 'fail',
+//       message: 'Missing name or price',
+//     });
+//   }
+//   next();
+// };
